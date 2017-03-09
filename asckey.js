0a7ba745fe73b031c94c556bc391621a0984dc5e
Luc Vergnes <lucvergnes@gmail.com>
2017-03-09 08:59:00 +0100

    AscKey-16 V2.2.0
    Added extra security and shorter outputs. Also added comments to better understand the code.

--- asckey.js
+++ asckey.js
@@ -1,20 +1,29 @@
+//   ###   AscKey encode 2.2.0   ###
+
 //Created by Luc Vergnes
-//AscKey encode 6.0.3
+//Fin this project on Github: https://github.com/lucvergnes/AscKey-16-text-obfuscator.git
 
-function Encode(input){
 
+//Encrypting function.
+function Encrypt(input){
+
+    //Create output and key variables
     var output = "";
     var key = "";
 
-    var rand1 = 400 + (Math.floor(Math.random() * 4000));
-    var rand2 = 500 + (Math.floor(Math.random() * 500));
-    var rand3 = 200 + (Math.floor(Math.random() * 100));
+    //Make four random numbers for the keys
+    var rand1 = 5 + (Math.floor(Math.random() * 5));
+    var rand2 = 125 + (Math.floor(Math.random() * 125));
+    var rand3 = 50 + (Math.floor(Math.random() * 50));
     var rand4 = 6 + (Math.floor(Math.random() * 4));
 
+    //Make a random character variable
     var randch = [];
 
+    //Set the different characters for the key (just because it looks nice)
     var seperators = ['f','g','c','a','e','d'];
 
+    //Now add one or two randomly selected characters between the numbers in the key
     for(var j = 0; j < 3; j ++) {
         var size = 1 + Math.floor(Math.random() * 2);
         var str = "";
@@ -24,41 +33,57 @@ function Encode(input){
         randch[j] = str;
     }
 
+    //Make the final key string
     key += rand1.toString() + randch[0] + rand2.toString() + randch[1] + rand3.toString() + randch[2] + rand4.toString();
 
+    //Now encrypt each character with the key and add it to the output string
     for(var i = 0; i < input.length; i ++) {
 
         var currentasc = input.charCodeAt(i);
         var outputasc = currentasc ;
-        outputasc += rand1;
+        outputasc = outputasc * rand1;
         outputasc += rand2;
         outputasc += rand3;
         outputasc = outputasc * rand4;
         output += outputasc.toString() + ' ';
     }
 
+    //Delete the last ' ' that was added
     output = output.substring(0,output.length-1);
 
+    //Make an array with the output and the key
     var out = [output,key];
+
+    //Return the array
     return out;
 }
 
-function Decode(input,key){
+//Decrypting function
+function Decrypt(input,key){
+
+    //Create output variable
     var output = "";
 
+    //Split the key with the g f c a e and d characters
     var keyval = key.split(/[gfcaed]+/);
+
+    //Split the input with spaces (' ')
     var charvalin = input.split(' ');
 
+    //Decode each number in the input string with the key
     for(var i = 0; i < charvalin.length; i ++){
 
         var charvalout = charvalin[i];
         charvalout = charvalout / keyval[3];
         charvalout -= keyval[2];
         charvalout -= keyval[1];
-        charvalout -= keyval[0];
+        charvalout = charvalout / keyval[0];
 
+        //Add it to the output string
         output += String.fromCharCode(charvalout);
     }
-    
+
+    //Return the output
     return output;
 }
+
